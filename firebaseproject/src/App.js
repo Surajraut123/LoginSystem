e2ca5e172c9b19cd76f04d480095453c1b240117
Suraj Rajendra Raut <rautsuraj345@gmail.com>
2024-04-08 00:48:17 +0530

    Created an authentication with firebase without dynmic ui

---
+++ firebaseproject/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import { Auth } from "./components/Auth"
+function App () {
+    return (
+        <div className="App">
+            <Auth/>
+        </div>
+    )
+}
+export default App;
